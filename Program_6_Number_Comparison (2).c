09232526082ea2706a2a2247b07b5b1d8d8e81a1
Bhagesh.c <bhageshc51@gmail.com>
2026-04-19 22:50:56 +0530

    Add number comparison program in C

---
+++ Program_6_Number_Comparison (2).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        printf("First number is greater than second number");
+    }
+    else if (num1 < num2) {
+        printf("Second number is greater than first number");
+    }
+    else {
+        printf("Both numbers are equal");
+    }
+
+    return 0;
+}
